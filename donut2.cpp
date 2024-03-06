dace9c80dc35cc68e78b25db3ee0e8108038c1e5
Hayden Dippel <hdippel@wisc.edu>
2024-03-06 12:24:58 -0600

    pushing donut2 proto

--- donut2.cpp
+++ donut2.cpp
@@ -1,4 +1,7 @@
 #include<vector>
+#include<cmath>
+#include<chrono>
+#include<iostream>
 
 using namespace std;
 
@@ -19,36 +22,47 @@ struct coord3D {
     double x;
     double y;
     double z;
+
+    bool operator==(const coord3D other) const {
+        const double tolerance = 1e-9;
+        return abs(x - other.x) < tolerance && abs(y - other.y) < tolerance && abs(z - other.z) < tolerance;
+    }
 };
 
-vector<coord3D> vector_intersects_donut() {
+vector<coord3D> vector_intersects_donut(coord3D& loc, coord3D& dir) {
+    // x = loc.x + dir.x * t
+    // y = loc.y + dir.y * t
+    // z = loc.z + dir.z * t
+
     
+
+
+
     return {};
 }
 
-double distance(coord3D a, coord3D b) {
-
-    return 0.0;
+double distance(coord3D& a, coord3D& b) {
+    return sqrt(a.x * b.x + a.y * b.y + a.z * b.z);
 }
 
 // https://web.cs.ucdavis.edu/~amenta/s12/findnorm.pdf
-coord3D get_normal(coord3D point) {
+coord3D get_normal(coord3D& point) {
 
     return {};
 }
 
 // cos(theta) = a.dot(b) / (magnitude(a) * magnitude(b))
-float angle_between_vectors(coord3D a, coord3D b) {
+float angle_between_vectors(coord3D& a, coord3D& b) {
 
     return 0.0;
 }
 
-void render_ascii_donut(coord3D light, coord3D view) {
+void render_ascii_donut(coord3D& light, coord3D& view) {
 
 }
 
 // quarternion or rotation matrix
-coord3D rotate_point(coord3D point) {
+coord3D rotate_point(coord3D& point) {
 
     return {};
 }
