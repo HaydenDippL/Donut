07900d37ae19f591583492b515902ef627fdeec3
Hayden Dippel <hdippel@wisc.edu>
2024-03-10 23:48:30 -0500

    almost completed, moved donut2 to donut

--- donut.cpp
+++ donut.cpp
@@ -1,92 +1,300 @@
-#include<iostream>
 #include<vector>
 #include<cmath>
-#include<unistd.h>
+#include<chrono>
+#include<iostream>
+#include<string>
+#include<limits>
+#include<thread>
+#include<stdlib.h>
 
 using namespace std;
 
+// MODIFY
+const int FPS = 20;
 const double R = 4.0;
 const double r = 2.2;
 const int WINDOW_WIDTH_CHAR = 61;
 const int WINDOW_HEIGHT_CHAR = 31;
 
+// DO NOT MODIFY
 const int RAYS_PER_UNIT_X = 4;
 const int RAYS_PER_UNIT_Y = 2;
-
 const double WINDOW_HEIGHT = 2.5 * (R + r);
 const double WINDOW_WIDTH = 2.5 * (R + r);
-
-const double X_MAX = WINDOW_WIDTH_CHAR / (2 * RAYS_PER_UNIT_X);
-const double X_MIN = -1 * X_MAX;
-const double Y_MAX = WINDOW_HEIGHT_CHAR / (2 * RAYS_PER_UNIT_Y);
-const double Y_MIN = -1 * Y_MAX;
 
 struct coord3D {
     double x;
     double y;
     double z;
+
+    coord3D(double x, double y, double z) : x(x), y(y), z(z) {}
+
+    bool operator==(const coord3D other) const {
+        const double tolerance = 1e-2;
+        return abs(x - other.x) < tolerance && abs(y - other.y) < tolerance && abs(z - other.z) < tolerance;
+    }
+
+    coord3D operator-(const coord3D other) const {
+        return {x - other.x, y - other.y, z - other.z};
+    }
+
+    coord3D operator+(const coord3D other) const {
+        return {x + other.x, y + other.y, z + other.z};
+    }
+
+    double operator*(const coord3D other) const {
+        return x * other.x + y * other.y + z * other.z;
+    }
+
+    void normalize() {
+        double magnitude = sqrt(x * x + y * y + z * z);
+        x /= magnitude;
+        y /= magnitude;
+        z /= magnitude;
+    }
 };
 
-
-
-vector<double> calc_z(double x, double y) {
-    double pos_d = sqrt(pow(x, 2) + pow(y, 2));
-    double neg_d = -1 * pos_d;
-
-    vector<double> ans;
-    double base;
-    if ((base = pow(r, 2) - pow(pos_d - R, 2)) >= 0) {
-        ans.push_back(sqrt(base));
-        ans.push_back(-sqrt(base));
-    } if ((base = pow(r, 2) - pow(neg_d - R, 2)) >= 0) {
-        ans.push_back(sqrt(base));
-        ans.push_back(-sqrt(base));
-    } return ans;
+// (x^2 + y^2 + z^2 + R^2 + r^2)^2 -4R^2 * (x^2 + y^2) < 0
+bool point_inside_donut(coord3D p) {
+    static double _4R2 = 4 * R * R;
+    static double R2_minus_r2 = R * R - r * r;
+    double x2_plus_y2 = p.x * p.x + p.y * p.y;
+    double inside_sq = (x2_plus_y2 + p.z * p.z + R2_minus_r2);
+    return inside_sq * inside_sq - _4R2 * x2_plus_y2 <= 0.0;
 }
 
-coord3D get_normal(coord3D coord) {
-    double op = sqrt(coord.x * coord.x + coord.z * coord.z);
-    double x = 2 * coord.x * (op - R) / op;
-    double y = 2 * coord.y;
-    double z = 2 * coord.z * (op - R) / op;
-    return {x, y, z};
+coord3D vector_intersects_donut(coord3D& loc, coord3D& dir) {
+    // r(t) = loc + dir * t
+    // x = loc.x + dir.x * t
+    // y = loc.y + dir.y * t
+    // z = loc.z + dir.z * t
+    static const double jump = 0.1;
+    static const double jump_jump = 0.01;
+    static const double R_plus_r = R + r;
+    static const double _R_minus_r = -R_plus_r;
+    static const double _r = -r;
+
+    double t1 = numeric_limits<double>::infinity(), t2 = numeric_limits<double>::infinity();
+
+    static auto min2 = [](double t, double& t1, double& t2) {
+        if (t < t1) {
+            t2 = t1;
+            t1 = t;
+        } else if (t < t2) {
+            t2 = t;
+        }
+    };
+
+    double temp;
+    double x, y, z;
+    if (dir.x != 0) {
+        temp = (R_plus_r - loc.x) / dir.x;
+        y = loc.y + dir.y * temp, z = loc.z + dir.z * temp;
+        if (y <= R_plus_r && y >= _R_minus_r && z <= r && z >= _r)
+            min2(temp, t1, t2);
+        temp = (_R_minus_r - loc.x) / dir.x;
+        y = loc.y + dir.y * temp, z = loc.z + dir.z * temp;
+        if (y <= R_plus_r && y >= _R_minus_r && z <= r && z >= _r)
+            min2(temp, t1, t2);
+    } if (dir.y != 0) {
+        temp = (R_plus_r - loc.y) / dir.y;
+        x = loc.x + dir.x * temp, z = loc.z + dir.z * temp;
+        if (x <= R_plus_r && x >= _R_minus_r && z <= r && z >= _r)
+            min2(temp, t1, t2);
+        temp = (_R_minus_r - loc.y) / dir.y;
+        x = loc.x + dir.x * temp, z = loc.z + dir.z * temp;
+        if (x <= R_plus_r && x >= _R_minus_r && z <= r && z >= _r)
+            min2(temp, t1, t2);
+    } if (dir.z != 0) {
+        temp = (r - loc.z) / dir.z;
+        x = loc.x + dir.x * temp, y = loc.y + dir.y * temp;
+        if (x <= R_plus_r && x >= _R_minus_r && y <= R_plus_r && y >= _R_minus_r)
+            min2(temp, t1, t2);
+        temp = (_r - loc.z) / dir.z;
+        x = loc.x + dir.x * temp, y = loc.y + dir.y * temp;
+        if (x <= R_plus_r && x >= _R_minus_r && y <= R_plus_r && y >= _R_minus_r)
+            min2(temp, t1, t2);
+    }
+
+    double t = t1;
+
+    while (t < t2 && !point_inside_donut({loc.x + dir.x * t, loc.y + dir.y * t, loc.z + dir.z * t})) {
+        t += jump;
+    } if (t >= t2) {
+        return {numeric_limits<double>::infinity(), numeric_limits<double>::infinity(), numeric_limits<double>::infinity()};
+    } while (point_inside_donut({loc.x + dir.x * t, loc.y + dir.y * t, loc.z + dir.z * t})) {
+        t -= jump_jump;
+    }
+    
+    return {loc.x + dir.x * t, loc.y + dir.y * t, loc.z + dir.z * t};
 }
 
-int main() {
+double distance(coord3D& a, coord3D& b) {
+    return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2) + pow(a.z - b.z, 2));
+}
 
-    coord3D light_source = {8.0, -10.0, 15.0};
-    vector<vector<double>> light_matrix(WINDOW_HEIGHT_CHAR, vector<double>(WINDOW_WIDTH_CHAR));
+// https://web.cs.ucdavis.edu/~amenta/s12/findnorm.pdf
+coord3D get_normal(coord3D& point) {
+    // estimate theta and phi
+    static double R2 = R * R;
+    static double _2PI = 2 * M_PI;
+    static double _3_over_2_PI = 3 * M_PI_2;
+    double phi = atan2(point.y, point.x);
+    double theta = asin(point.z / r);
+    bool upper = point.z > 0;
+    bool right = point.x * point.x + point.y * point.y > R2;
+    if (point.z == 0.0 && right) {
+        theta = 0;
+    } else if (point.z == r) {
+        theta = M_PI_2;
+    } else if (point.z == 0.0 && !right) {
+        theta = M_PI;
+    } else if (point.z == -r) {
+        theta = _3_over_2_PI;
+    } else if (upper && right) {
+        theta = theta;
+    } else if (upper && !right) {
+        theta = M_PI - theta;
+    } else if (!upper && !right) {
+        theta = M_PI - theta;
+    } else {
+        theta = _2PI + theta;
+    }
 
-    double y = Y_MIN;
-    for (int h = 0; h < WINDOW_HEIGHT_CHAR; ++h) {
-        double x = X_MIN;
-        for (int w = 0; w < WINDOW_WIDTH_CHAR; ++w) {
-            vector<double> zs = calc_z(x, y);
-            if (zs.size() == 0) {
-                light_matrix[h][w] = 0;
+    // get normal from theta and phi
+    double tx = -sin(phi);
+    double ty = cos(phi);
+    double tz = 0;
+    double sx = cos(phi) * -sin(theta);
+    double sy = sin(phi) * -sin(theta);
+    double sz = cos(theta);
+    double nx = ty * sz - tz * sy;
+    double ny = tz * sx - tx * sz;
+    double nz = tx * sy - ty * sx;
+
+    coord3D normal = {nx, ny, nz};
+    normal.normalize();
+    return normal;
+}
+
+// cos(theta) = a.dot(b) / (magnitude(a) * magnitude(b))
+// requires two normalized vectors
+float angle_between_vectors(coord3D& a, coord3D& b) {
+    double dot = -a.x * b.x - a.y * b.y - a.z * b.z;
+    return acos(dot);
+}
+
+char light_to_char(double angle_degrees) {
+    static const string light_chars = ".,-~:;=!*#$@";
+    static const int num_light_chars = light_chars.size();
+    static const double scale = 90.0 / 11.9;
+    if (angle_degrees >= 90.0) return '.';
+    int index = int((90.0 - angle_degrees) / scale);
+    return light_chars[index];
+}
+
+char* render_ascii_donut(coord3D& light, coord3D& view, double A, double B) {
+    static const double x_min = -WINDOW_WIDTH_CHAR / (2 * RAYS_PER_UNIT_X);
+    static const double x_max = -x_min;
+    static const double y_min = -WINDOW_HEIGHT_CHAR / (2 * RAYS_PER_UNIT_Y);
+    static const double y_max = -y_min;
+    static const double x_inc = 1.0 / RAYS_PER_UNIT_X;
+    static const double y_inc = 1.0 / RAYS_PER_UNIT_Y;
+    static const double to_degrees = 180.0 / M_PI;
+    static const int total_chars = WINDOW_HEIGHT_CHAR * (WINDOW_WIDTH_CHAR + 1) + 1;
+
+    const double cosA = cos(A);
+    const double sinA = sin(A);
+    const double nsinA = -sinA;
+    const double cosB = cos(B);
+    const double sinB = sin(B);
+    const double nsinB = -sinB;
+
+    coord3D light_rotated = {
+        light.x * cosB - sinB * (light.y * cosA - light.z * sinA),
+        light.x * sinB + cosB * (light.y * cosA - light.z * sinA),
+        light.y * sinA + light.z * cosA
+    };
+
+    coord3D view_rotated = {
+        view.x * cosB - sinB * (view.y * cosA - view.z * sinA),
+        view.x * sinB + cosB * (view.y * cosA - view.z * sinA),
+        view.y * sinA + view.z * cosA
+    };
+
+    coord3D view_dir_rotated = coord3D(0, 0, 0) - view_rotated;
+    view_dir_rotated.normalize();
+
+    char* buffer = new char[total_chars];
+    int i = 0;
+    for (double y = y_max; y >= y_min; y -= y_inc) {
+        for (double x = x_min; x <= x_max; x += x_inc) {
+            double inside_paren = y * cosA - view.z * sinA;
+            coord3D view_loc_rotated = {x * cosB - sinB * inside_paren, x * sinB + cosB * inside_paren, y * sinA + view.z * cosA};
+
+            coord3D view_intersect_point = vector_intersects_donut(view_loc_rotated, view_dir_rotated);
+            bool vector_intersecting_donut = view_intersect_point.z != numeric_limits<double>::infinity();
+            if (!vector_intersecting_donut) {
+                buffer[i++] = ' ';
                 continue;
             }
-            double z = numeric_limits<double>::lowest();
-            for (const double potential_z : zs) z = max(z, potential_z);
 
-            light_matrix[h][w] = calc_z(x, y).size() != 0;
-            x += 1.0 / RAYS_PER_UNIT_X;
-        } y += 1.0 / RAYS_PER_UNIT_Y;
+            coord3D light_dir_to_donut = view_intersect_point - light_rotated;
+            light_dir_to_donut.normalize();
+            coord3D light_intersect_point = vector_intersects_donut(light_rotated, light_dir_to_donut);
+            if (light_intersect_point.x == numeric_limits<double>::infinity() || !(view_intersect_point == light_intersect_point)) {
+                buffer[i++] = '.';
+                continue;
+            }
+
+            coord3D normal = get_normal(view_intersect_point);
+            double angle_degrees = angle_between_vectors(normal, light_dir_to_donut) * to_degrees;
+            buffer[i++] = light_to_char(angle_degrees);
+        } buffer[i++] = '\n';
+    } buffer[i] = '\0';
+
+    return buffer;
+}
+
+chrono::milliseconds get_time() {
+    return chrono::duration_cast<chrono::milliseconds>(
+        chrono::system_clock::now().time_since_epoch()
+    );
+}
+
+// torus has x max 5 and y max 5
+// donut along x and y axis
+// theta is around circle -> r
+// phi is around donut -> R
+int main() {
+    static const auto ms_in_frame = chrono::milliseconds(1000 / FPS);
+    static const double inc_A = 3.0 * M_PI / 180.0;
+    static const double inc_B = 2.0 * M_PI / 180.0;
+    static const double _2PI = 2.0 * M_PI;
+    
+    coord3D light = {0, 5, -15};
+    coord3D view = {0, 0, -10};
+    double A = 0.0;
+    double B = 0.0;
+    int frame = 1;
+    while (true) {
+        chrono::milliseconds start_frame_ms = get_time();
+        // clear screen
+        char* donut = render_ascii_donut(light, view, A, B);
+        chrono::milliseconds end_render_ms = get_time();
+        chrono::milliseconds render_duration_ms = end_render_ms - start_frame_ms;
+        chrono::milliseconds sleep_duration_ms = ms_in_frame - render_duration_ms;
+        if (sleep_duration_ms.count() > 0) this_thread::sleep_for(sleep_duration_ms);
+        
+        if (frame++ > 1) system("clear");
+        cout << donut;
+
+        A += inc_A;
+        B += inc_B;
+        if (A > _2PI) A -= _2PI;
+        if (B > _2PI) B -= _2PI;
     }
-
-    for (int h = 0; h < WINDOW_HEIGHT_CHAR; ++h) {
-        for (int w = 0; w < WINDOW_WIDTH_CHAR; ++w) {
-            cout << (light_matrix[h][w] ? '#' : ' ');
-        } cout << '\n';
-    }
-
-    for (int h = 0; h < WINDOW_HEIGHT_CHAR; ++h) {
-        for (int w = 0; w < WINDOW_WIDTH_CHAR; ++w) {
-            cout << (light_matrix[h][w]);
-        } cout << '\n';
-    }
-
-    // render the donut with print statements
+    
 
     return 0;
 }
